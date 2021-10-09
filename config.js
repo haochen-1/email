37af961000e6997417bf37d8912d290bc52b15c8
Zhuhaochen <329733395@qq.com>
2021-10-09 22:28:12 +0800

    上传了

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '皓辰提醒你哟', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '329733395@qq.com', // 发送者邮箱
+  pass: 'ulejtkphfvnebjga', // 发送者邮箱MTP协议密码
+  to: '2416865410@qq.com', // 发送到谁，填邮箱
+  weatherKey: '12cd785d3c7e4d92ba78a9500e1c87a2', // 和风天气key
+  location: '101300112',  // 和风天气-成都武侯区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: '9a6f482ed3ee8995db72b7eeefcedbfb', // 天行数据的key
+  startDay: '2020-09-03', // 在一起的日期
+}
