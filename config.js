adfbf6a5be26437e0ff8ad38513902682f9f8d15
Zhuhaochen <329733395@qq.com>
2021-10-10 16:46:46 +0800

    修改了配置

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: '329733395@qq.com', // 发送者邮箱
   pass: 'ulejtkphfvnebjga', // 发送者邮箱MTP协议密码
-  to: '329733395@qq.com', // 发送到谁，填邮箱
+  to: '2416865410@qq.com', // 发送到谁，填邮箱
   weatherKey: '12cd785d3c7e4d92ba78a9500e1c87a2', // 和风天气key
   location: '101300112',  // 和风天气-成都武侯区的id
   type: '1,3,9', // 和风天气-生活指数type
